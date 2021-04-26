ae11a4d9c11d2bcd8dd1da6270e9967b9dab50fd
d-martinelli1612 <d.martinelli1612@gmail.com>
2021-04-26 19:18:30 +0200

    Riporto
    Sto riportando nella cartella principale

---
+++ src/it/unibs/Pa/codiceFiscale/InputCodiciFiscali.java
@@ -0,0 +1,16 @@
+/*Classe per la lettura dei dati presenti nei file XML: comuni; inputPersone; codiciFiscali*/
+
+package it.unibs.Pa.codiceFiscale;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+public class InputCodiciFiscali {
+    XMLInputFactory xmlif = null;
+    XMLStreamReader xmlr = null;
+    /*try{
+        xmlif = XMLInputFactory.newInstance();
+    } catch (Exception e) {
+
+    }*/
+}
---
+++ src/it/unibs/Pa/codiceFiscale/Persona.java
@@ -0,0 +1,4 @@
+package it.unibs.Pa.codiceFiscale;
+
+public class Persona {
+}
